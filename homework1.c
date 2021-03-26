8563fe5b10015aa8207c39fd0ddd1faed16f716c
HamBP <sjy_9484@naver.com>
2021-03-27 05:07:48 +0900

    modify

--- homework1.c
+++ homework1.c
@@ -2,30 +2,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 5
+
 int main(int argc, char** argv) {
 	int fd[2];
-	char buffer[10];
+	char buffer[BUFFER_SIZE];
 	pid_t pid;
-	int state;
 
 	if(pipe(fd) == -1) {
-		printf("pipe() errov\n");
+		printf("pipe() error!\n");
 		exit(1);
 	}
-
+	
 	pid = fork();
 	if(pid == -1) {
 		printf("fork() error!\n");
 		exit(1);
 	}
-
-	if(pid == 0) { // child
+	// child
+	if(pid == 0) {
 		printf("hello\n");
-		write(fd[1], "1", 10);
+		write(fd[1], "", BUFFER_SIZE);
+		close(fd[1]);
 	}
-	else { // parent
-		read(fd[0], buffer, 10);
+	// parent
+	else {
+		read(fd[0], buffer, BUFFER_SIZE);
 		printf("goodbye\n");
+		close(fd[0]);
 	}
 
 	return 0;
